d001aa39888422a836b453697839d9bb889aa2e7
sivakusi <sivakusi@fsjars.com>
2019-04-03 18:54:25 +0530

    [Dev] added files and wav conversions

--- ex.py
+++ ex.py
@@ -1,11 +1,105 @@
-a = int(input("enter first number: "))
-b = int(input("enter second number: "))
+# a = int(input("enter first number: "))
+# b = int(input("enter second number: "))
  
-sum = a + b
+# sum = a + b
  
-print(sum)
+# print(sum)
 
-
+import os.path
 import rpy2 as R
+# import base
 
-print(R)
+import rpy2.robjects as robjects
+from rpy2.robjects import default_py2ri as hello
+import rpy2.rlike.container as rlc
+from rpy2.robjects import pandas2ri
+
+# import pandas.rpy2.common as com
+
+r = robjects.r
+
+
+output = r.source('./ex.R')
+
+from rpy2.robjects.packages import importr
+from rpy2.robjects.methods import RS4
+
+# utils = importr('utils')
+# utils.chooseCRANmirror(ind=1)
+# utils.install_packages('seewave')
+
+warbleR = importr('warbleR')
+tuneR = importr('tuneR')
+seewave = importr('seewave')
+base = importr('base')
+
+start = int(0)
+end = int(20)
+df = robjects.DataFrame({})
+data = rlc.OrdDict([('sound.files', './testFiles/sine.wav'),
+                    ('selec', 0),
+                    ('start', start),
+                    ('end', end)])
+
+# data = {'sound.files' : './testFiles/sine.wav', 'selec' : 0, 'start' : robjects.IntVector(0), 'end': robjects.IntVector(20)}
+df = robjects.DataFrame(data)
+print(df)
+
+b = robjects.IntVector([0, 22])
+
+tuneR_r = tuneR.readWave(os.path.join('./testFiles/sine.wav'), 0, to = 20, units = "seconds")
+# tuneR_rR = r('''tuneR_r''')
+# res = r('''resultdf <- as.data.frame(tuneR_rR)''')
+dollar = base.__dict__["$"]
+
+# print(tuneR_r@samp.rate)
+wavspec = seewave.spec(tuneR_r, plot = False)
+wavspecprop = seewave.specprop(wavspec, plot = False)
+print(tuneR_r)
+print(wavspecprop[0][0])
+
+mean = wavspecprop[0][0]/1000
+sd = wavspecprop[1][0]/1000
+median = wavspecprop[2][0]/1000
+sem = wavspecprop[3][0]
+mode = wavspecprop[4][0]/1000
+Q25 = wavspecprop[5][0]/1000
+Q75 = wavspecprop[6][0]/1000
+IQR = wavspecprop[7][0]/1000
+centroid = wavspecprop[8][0]/1000
+skew = wavspecprop[9][0]
+kurt = wavspecprop[10][0]
+sfm = wavspecprop[11][0]
+sh = wavspecprop[12][0]
+
+ylim = robjects.IntVector((0, 280/1000))
+
+wavefundf = seewave.fund(wavspec, f = 16000, ovlp = 50, fmax = 280, ylim = ylim, plot = False)
+# print(wavefundf)
+
+spe = seewave.dfreq(wavspec, f = 16000, plot = False)
+print(spe)
+
+# print(dollar(tuneR_r, "Samplingrate"))
+
+
+
+# warbleR.specan(df , bp = "frange", wl = 2048, threshold = 5, parallel = 1)
+
+# print('hello')
+
+
+# output1 = r.source('server.R')
+
+# import wave, struct
+
+# waveFile = wave.open('sine.wav', 'r')
+
+# length = waveFile.getnframes()
+# for i in range(0,length):
+#     waveData = waveFile.readframes(1)
+#     data = struct.unpack("<h", waveData)
+#     print(int(data[0]))
+
+# output.specan3(['sound.files', 'selec', 'start', 'end'], parallel =1)
+# print(output.specan3(X = df))
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix 
 from sklearn.model_selection import train_test_split 
@@ -21,10 +21,12 @@ from sklearn.metrics import classification_report
 
 
 FLAGS = None
+labels = None
 
 def map_target(data, fieldName):
     data_modified = data.copy()
     targets = data[fieldName].unique()
+    labels = targets
     mapper = {
         name : n for n,
         name in enumerate(targets)
@@ -59,14 +61,14 @@ def splitdataset(data, label):
 # Function to make predictions 
 def prediction(X_test, clf_object): 
   
-    # Predicton on test with giniIndex 
+    # Prediction on test with giniIndex 
     y_pred = clf_object.predict(X_test) 
     print("Predicted values:") 
     print(y_pred) 
     return y_pred 
       
 # Function to calculate accuracy 
-def cal_accuracy(y_test, y_pred): 
+def cal_accuracy(y_test, y_pred, targets): 
       
     print("Confusion Matrix: \n", 
         confusion_matrix(y_test, y_pred)) 
@@ -75,7 +77,10 @@ def cal_accuracy(y_test, y_pred):
     accuracy_score(y_test,y_pred)*100) 
       
     print("Report : \n", 
-     classification_report(y_test, y_pred)) 
+     classification_report(y_test, y_pred, target_names=targets))
+
+def integrated(dataset, actual):
+    print(actual)
   
 def main(_):
     # print(FLAGS.data)
@@ -84,15 +89,15 @@ def main(_):
     # print(data_set.head())
     # print(data_set["label"].unique())
     data_set_processed, targets = map_target(data_set, "label")
-    print(data_set_processed.head())
-    print(data_set_processed.tail())
-    # print(targets)
+    # print(data_set_processed.head())
+    # print(data_set_processed.tail())
+    print(targets)
     features = list(data_set_processed.columns[:20])
     print(features)
     y = data_set_processed["label"]
-    print(y.head())
+    # print(y.head())
     x = data_set_processed[features]
-    print(x)
+    # print(x)
     x_train, x_test, y_train, y_test = splitdataset(x, y)
     # print("Training values")
     # print(x_train)
@@ -110,25 +115,35 @@ def main(_):
     # print(CART_pred)
     CART_train_pred =  CART_train.predict(x_train)
     # visualize_tree(CART_train, features, 'CART.dot')
-    cal_accuracy(y_train, CART_train_pred)
-    cal_accuracy(y_test, CART_pred)
+    print("Training Measures")
+    cal_accuracy(y_train, CART_train_pred, targets)
+    print("Testing Measures")
+    cal_accuracy(y_test, CART_pred, targets)
 
     print('\n\n SVM \n\n')
     SVM_train = svm.SVC(gamma='auto')
     SVM_train.fit(x_train, y_train)
+    SVM_train_pred = SVM_train.predict(x_train)
     SVM_pred = SVM_train.predict(x_test)
-    print(SVM_pred)
+    # print(SVM_pred)
     # visualize_tree(SVM_train, features, 'SVM.dot')
-    cal_accuracy(y_test, SVM_pred)
+    print("Training Measures")
+    cal_accuracy( y_train , SVM_train_pred, targets)
+    print("Testing Measures")
+    cal_accuracy(y_test, SVM_pred, targets)
 
 
     print('\n\n RFC \n\n')
     RFC_train = RandomForestClassifier(n_estimators=10)
-    RFC_train.fit(x_train, y_train) 
+    RFC_train.fit(x_train, y_train)
+    RFC_train_pred = RFC_train.predict(x_train)
     RFC_pred = RFC_train.predict(x_test)
     # print(RFC_pred)
     # visualize_tree(RFC_train, features, 'RFC.dot')
-    cal_accuracy(y_test, RFC_pred)
+    print("Training Measures")
+    cal_accuracy( y_train , RFC_train_pred, targets)
+    print("Testing Measures")
+    cal_accuracy(y_test, RFC_pred, targets)
 
 
     print('\n\n XGB \n\n')
@@ -141,21 +156,52 @@ def main(_):
                                 {'learning_rate': 0.01, 'subsample': 1.0})]:
         params = dict(original_params)
         params.update(setting)
-        XGB = GradientBoostingClassifier(**params)
-        XGB.fit(x_train, y_train)
-    XGB_pred = XGB.predict(x_test)
+    XGB_train = GradientBoostingClassifier(**params)
+    XGB_train = XGB_train.fit(x_train, y_train)
+    XGB_train_pred = XGB_train.predict(x_train)
+    XGB_pred = XGB_train.predict(x_test)
     # print(XGB_pred)
-    cal_accuracy(y_test, XGB_pred)
+    print("Training Measures")
+    cal_accuracy( y_train , XGB_train_pred, targets)
+    print("Testing Measures")
+    cal_accuracy(y_test, XGB_pred, targets)
 
-    print('\n\n MLP \n\n')
-    mlp = MLPClassifier(hidden_layer_sizes=(100,10), max_iter=1000, alpha=1e-4,
-                        solver='adam', verbose=1, tol=1e-4, random_state=None, learning_rate_init=.001)
+    print('\n\n ADA \n\n')
+    ADA_train =  AdaBoostClassifier(n_estimators=50, learning_rate=1)
 
-    mlp.fit(x_train, y_train)
-    MLP_pred = mlp.predict(x_test)
-    # print(MLP_pred)
-    cal_accuracy(y_test, MLP_pred)
+    ADA_train.fit(x_train, y_train)
+    ADA_train_pred = ADA_train.predict(x_train)
+    ADA_pred = ADA_train.predict(x_test)
+    # print(ADA_pred)
+    print("Training Measures")
+    cal_accuracy( y_train , ADA_train_pred, targets)
+    print("Testing Measures")
+    cal_accuracy(y_test, ADA_pred, targets)
 
+    int_train = [ CART_train_pred, SVM_train_pred, RFC_train_pred, XGB_train_pred, ADA_train_pred, y_train ]
+    int_test = [ CART_pred, SVM_pred, RFC_pred, XGB_pred, ADA_pred, y_test ]
+    int_data_columns = ['CART', 'SVM', 'RFC', 'XGB', 'ADA', 'label']
+
+    train_data = { 'CART' : CART_train_pred, 'SVM' : SVM_train_pred, 'RFC' : RFC_train_pred, 'XGB' : XGB_train_pred, 'ADA' : ADA_train_pred}
+    test_data = { 'CART' : CART_pred, 'SVM' : SVM_pred, 'RFC' : RFC_pred, 'XGB' : XGB_pred, 'ADA' : ADA_pred }
+    int_train_data = pd.DataFrame(train_data)
+    int_test_data = pd.DataFrame(test_data)
+
+    # print(int_train_data)
+    # print(int_test_data)
+
+    
+    print('\n\n INT \n\n')
+    INT_train = RandomForestClassifier(n_estimators=10)
+    INT_train.fit(int_train_data, y_train)
+    INT_train_pred = INT_train.predict(int_train_data)
+    INT_pred = INT_train.predict(int_test_data)
+    # print(INT_pred)
+    # visualize_tree(INT_train, features, 'INT.dot')
+    print("Training Measures")
+    cal_accuracy( y_train , INT_train_pred, targets)
+    print("Testing Measures")
+    cal_accuracy(y_test, INT_pred, targets)
 
 
 if __name__ == "__main__":
